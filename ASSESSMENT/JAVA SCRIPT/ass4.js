ef7121ffe1d5512bea7f7d8130a3494e068dfe99
DANGAROSIYAMARGIRAJESHBHAI <136425698+DANGAROSIYAMARGIRAJESHBHAI@users.noreply.github.com>
2024-02-20 12:15:36 +0530

    Add files via upload

--- ASSESSMENT/JAVA SCRIPT/ass4.js
+++ ASSESSMENT/JAVA SCRIPT/ass4.js
@@ -4,7 +4,8 @@
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
   const forms = document.querySelectorAll('.needs-validation')
-
+  let ptn = /^[A-Z a-z]{3,5}$/;
+  let name = document.getElementById('name').value;
   // Loop over them and prevent submission
   Array.from(forms).forEach(form => {
     form.addEventListener('submit', event => {
@@ -12,33 +13,7 @@
         event.preventDefault()
         event.stopPropagation()
       }
-
       form.classList.add('was-validated')
     }, false)
   })
 })()
-
-
-
-  
-
-        // Form validation
-        function validate() {
-            let ptn = /^[A-Z a-z]{3,5}$/;
-            let name = document.getElementById('name').value;
-          
-            console.log("Hello")
-            // Name validation
-            if (name == "" || name == ' ') {
-                name.style.outline = "1px solid red";
-                let msg = document.querySelector('#name~span')
-                name.innerHTML = "*Need to add name"
-                return false;
-            }
-            else if (!ptn.test(name)) {
-              alert('Enter proper username')
-               return false;
-            }
-        
-            }
-        
